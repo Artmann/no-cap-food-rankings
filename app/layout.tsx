2e3560eacfb8c03f50168364198edf50162bcbe8
Christoffer Artmann <artgaard@gmail.com>
2026-03-07 21:54:32 +0100

    OG props

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,33 @@ const fontMono = JetBrains_Mono({
   variable: '--font-mono'
 })
 
+const url = 'https://chow-where.vercel.app'
+
 export const metadata: Metadata = {
   description: 'Vote on which country has the best food.',
-  title: 'Best Chow Where?'
+  metadataBase: new URL(url),
+  openGraph: {
+    description: 'Vote on which country has the best food.',
+    images: [
+      {
+        alt: 'Best Chow? Vote Now!',
+        height: 630,
+        url: '/og.png',
+        width: 1200
+      }
+    ],
+    siteName: 'Best Chow Where?',
+    title: 'Best Chow Where?',
+    type: 'website',
+    url
+  },
+  title: 'Best Chow Where?',
+  twitter: {
+    card: 'summary_large_image',
+    description: 'Vote on which country has the best food.',
+    images: ['/og.png'],
+    title: 'Best Chow Where?'
+  }
 }
 
 export default function RootLayout({
