cd900dd98644fb51986414b6274f8c3b8b242ba4
Christoffer Artmann <artgaard@gmail.com>
2026-03-07 23:25:04 +0100

    Add Sentry error monitoring

---
+++ sentry.client.config.ts
@@ -0,0 +1,9 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://0c426d58e5a0f7625362cf8d0dab69db@o1110961.ingest.us.sentry.io/4511005529473024',
+  integrations: [Sentry.replayIntegration()],
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  tracesSampleRate: 1.0
+})
---
+++ sentry.edge.config.ts
@@ -0,0 +1,6 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://0c426d58e5a0f7625362cf8d0dab69db@o1110961.ingest.us.sentry.io/4511005529473024',
+  tracesSampleRate: 1.0
+})
