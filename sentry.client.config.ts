7fbc971002aeda69691197bb59ecb614a54262ae
Christoffer Artmann <artgaard@gmail.com>
2026-03-07 23:29:01 +0100

    Track environment in Sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: 'https://0c426d58e5a0f7625362cf8d0dab69db@o1110961.ingest.us.sentry.io/4511005529473024',
+  environment: process.env.NODE_ENV,
   integrations: [Sentry.replayIntegration()],
   replaysOnErrorSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,5 +2,6 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: 'https://0c426d58e5a0f7625362cf8d0dab69db@o1110961.ingest.us.sentry.io/4511005529473024',
+  environment: process.env.NODE_ENV,
   tracesSampleRate: 1.0
 })
