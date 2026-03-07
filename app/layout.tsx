9e816a66859e64c67d7cc483ec34c5c3d897db74
Christoffer Artmann <artgaard@gmail.com>
2026-03-07 21:42:18 +0100

    Install Shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { JetBrains_Mono, Merriweather, Outfit } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
+const fontSans = Outfit({
+  subsets: ['latin'],
+  variable: '--font-sans'
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+const fontSerif = Merriweather({
+  subsets: ['latin'],
+  variable: '--font-serif'
+})
+
+const fontMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono'
 })
 
 export const metadata: Metadata = {
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
       >
         {children}
       </body>
